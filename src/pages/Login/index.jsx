0749c0a819901994f151af5689d91f5b825db887
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-09-14 20:35:23 +0700

    cleaning code

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./login.css";
 import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
