a74cf001a08aaffd45ca37ee37182fa38ee3691e
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-09-14 20:59:47 +0700

    fix error: videoDetailsPage

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,17 +1,25 @@
-import { formatDistanceToNow } from "date-fns";
+export function formatDate(dateString) {
+	const currentDate = new Date();
+	const inputDate = new Date(dateString);
 
-export const formatDate = (dateString) => {
-	const parsedDate = new Date(dateString ?? null);
-	const timeAgo = formatDistanceToNow(parsedDate, {
-		addSuffix: true,
-		comparisonBase: new Date(),
-	});
+	const miliSeconds = currentDate - inputDate;
+	const seconds = Math.floor(miliSeconds / 1000);
+	const minutes = Math.floor(seconds / 60);
+	const hours = Math.floor(minutes / 60);
+	const days = Math.floor(hours / 24);
+	const months = Math.floor(days / 30);
 
-	if (timeAgo.startsWith("in ")) {
-		const removeWord = timeAgo?.replace("in ", "");
-
-		return removeWord + " ago";
+	if (months >= 1) {
+		return `${months} months ago`;
+	} else if (days >= 1) {
+		return `${days} days ago`;
+	} else if (hours >= 1) {
+		return `${hours} hours ago`;
+	} else if (minutes >= 1) {
+		return `${minutes} minutes ago`;
+	} else if (seconds >= 1) {
+		return `${seconds} seconds ago`;
+	} else {
+		return "Just now";
 	}
-
-	return timeAgo;
-};
+}
